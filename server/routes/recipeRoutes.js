bf438f2af28a61cbb7f0d645a39ce4d0c4cd6020
Farhat-Khan12 <90671950+Farhat-Khan12@users.noreply.github.com>
2022-04-25 17:07:39 +0530

    Add files via upload

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -6,5 +6,10 @@ const recipeController = require('../controllers/recipeController');
 router.get('/',recipeController.homepage);
 router.get('/categories',recipeController.exploreCategories);
 router.get('/recipe/:id',recipeController.exploreRecipe);
+router.get('/categories/:id',recipeController.exploreCategoriesById);
+router.post('/search',recipeController.searchRecipe);
+router.get('/explore-latest', recipeController.exploreLatest);
+router.get('/random-recipe', recipeController.showRecipe);
+
 
 module.exports = router;
