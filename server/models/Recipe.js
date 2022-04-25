788f26ee71304a175fe4cd6ceacdaf73605e1bb6
Farhat-Khan12 <90671950+Farhat-Khan12@users.noreply.github.com>
2022-04-25 17:08:31 +0530

    Add files via upload

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -1,1 +1,26 @@
-
+const mongoose = require("mongoose");
+const recipeSchema = new mongoose.Schema({
+    name:{
+        type: String, required: "This is a required field."
+    },
+    image:{
+        type: String, required: "This is a required field."
+    },
+    description:{
+        type: String, required: "This is a required field."
+    },
+    email:{
+        type: String, required: "This is a required field."
+    },
+    ingredients:{
+        type: Array, required: "This is a required field."
+    },
+    category:{
+        type: String, 
+        enum: ['Indian','Thai','American','Chinese','Mexican','Spanish','Italian'],
+        required: "This is a required field."
+    },
+});
+recipeSchema.index({name: 'text', description: 'text'});
+//reipeSchema.index({"$**" : "text"});
+module.exports = mongoose.model('Recipe', recipeSchema);
