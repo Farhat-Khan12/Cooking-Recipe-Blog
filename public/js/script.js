9ae44acdb6bce4ed865690053478c0511bee0287
Farhat-Khan12 <90671950+Farhat-Khan12@users.noreply.github.com>
2022-04-26 19:50:49 +0530

    Add files via upload

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,10 @@
+let addIngredientsBtn = document.getElementById('addIngredients');
+let ingredientList = document.querySelector('.ingredientList');
+let ingredientDiv = document.querySelectorAll('.ingredientDiv')[0];
 
+addIngredientsBtn.addEventListener('click',function(){
+    let newIngredients = ingredientDiv.cloneNode(true);
+    let input = newIngredients.getElementsByTagName('input')[0];
+    input.value = '';
+    ingredientList.appendChild(newIngredients);
+});
