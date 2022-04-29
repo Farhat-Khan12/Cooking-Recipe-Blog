450ddddc5ec9dc33a9017bb6122487d3f1a4c957
Farhat-Khan12 <90671950+Farhat-Khan12@users.noreply.github.com>
2022-04-29 19:22:37 +0530

    Add files via upload

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -12,14 +12,15 @@ exports.homepage = async (req, res) => {
     const italian = await Recipe.find({ category: "Italian" }).limit(limitNumber);
     const chinese = await Recipe.find({ category: "Chinese" }).limit(limitNumber);
     const mexican = await Recipe.find({ category: "Mexican" }).limit(limitNumber);
-    const food = { latest, indian, american, italian, chinese, mexican };
+    const indonesian = await Recipe.find({ category: "Indonesian" }).limit(limitNumber);
+    const food = { latest, indian, american, italian, chinese, mexican, indonesian};
     res.render("index", { title: "Cooking Blog - Home", categories, food });
   } catch (error) {
     res.status(500).send({ message: error.message || "Error Occured" });
   }
 };
 
-//GET /categories
+//GET /categories 
 exports.exploreCategories = async (req, res) => {
   try {
     limitNumber = 20;
@@ -107,21 +108,47 @@ exports.showRecipe = async (req, res) => {
 
 //GET /submit-recipe
 exports.submitRecipe = async (req, res) => {
-  try {
-    res.render("submit-recipe", { title: "Cooking Blog - Submit Recipe" });
-  } catch (error) {
-    res.status(500).send({ message: error.message || "Error Occured" });
-  }
+    const infoErrorsObj = req.flash('infoErrors');
+    const infoSubmitObj = req.flash('infoSubmit');
+    res.render("submit-recipe", { title: "Cooking Blog - Submit Recipe", infoErrorsObj, infoSubmitObj});
 };
 
 //POST /submit-recipe
 exports.submitRecipeOnPost = async (req, res) => {
-  try {
-    res.redirect("submit-recipe");
-  } catch (error) {
-    res.status(500).send({ message: error.message || "Error Occured" });
+  try{
+    let imageUploadFile;
+    let uploadPath;
+    let newImageName;
+    
+    if(!req.files || Object.keys(req.files).length === 0){
+      console.log('No Files were uploaded.');
+    }
+    else{
+      imageUploadFile = req.files.image;
+      newImageName = Date.now() + imageUploadFile.name;
+      uploadPath = require('path').resolve('./')+'/public/uploads/'+newImageName;
+      imageUploadFile.mv(uploadPath, function(err){
+        if(err) return res.status(500).send(err);
+      })
+    }
+
+    const newRecipe = new Recipe({
+      name: req.body.name,
+      description: req.body.description,
+      email: req.body.email,
+      ingredients: req.body.ingredients,
+      category: req.body.category,
+      image: newImageName
+    });
+    await newRecipe.save();
+    req.flash('infoSubmit','Recipe has been added.');
+    res.redirect("/submit-recipe");
+  }catch(error){
+    req.flash('infoErrors',error);
+    res.redirect('/submit-recipe');
   }
 };
+
 
 // async function insertData(){
 //     try{
